985729722a39a9138a3f9e0b4a23dc8c9857ac90
imrebartis <imre.bartis@siili.com>
2017-10-05 16:06:58 +0300

    leap year done

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -41,7 +41,11 @@
         )
     )
 
-(defn leap-year? [year]
-  ":(")
+(defn leap
+  [year]
+  (cond (zero? (mod year 400)) true
+        (zero? (mod year 100)) false
+        (zero? (mod year 4)) true
+        :default false))
 
 ; '_______'
